9a34e176df66390f91abb608151a3e648b0bcc02
Lenovo <Lenovo@LAPTOP-MN28959H>
2022-11-09 22:27:58 +0530

    first commit

---
+++ 7pmallure/src/main/java/pack1/c1.java
@@ -0,0 +1,22 @@
+package pack1;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class c1 {
+	
+	@Test
+	public void t1()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver ob= new ChromeDriver();
+		ob.get("http://www.google.com");
+		ob.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		System.out.println(ob.getTitle());//4
+		ob.navigate().refresh();//4
+		ob.findElementByLinkText("Images"); //4
+		ob.quit();//4
+	}
+
+}
